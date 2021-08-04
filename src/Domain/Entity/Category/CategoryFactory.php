a5b9c4eee3094c8fcc01023f783f2544e1cb61dd
Johnny Saymon <saymon@ciebit.com>
2021-08-03 21:44:13 -0300

    feat(entity): fabrica de categoria

---
+++ src/Domain/Entity/Category/CategoryFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Entity\Category;
+
+use App\Domain\Entity\Category\Attribute;
+use App\Domain\Entity\Category\AttributeName;
+use App\Domain\Entity\Category\AttributeCollection;
+use App\Domain\Entity\Category\Category;
+use App\Domain\Entity\Category\CategoryName;
+
+final class CategoryFactory
+{
+    public static function create(
+        string $name,
+        array $attributeList
+    ): Category
+    {
+        $attributeCollection = new AttributeCollection(
+            ...array_map(function(string $name) {
+                return new Attribute(
+                    new AttributeName($name)
+                );
+            }, $attributeList)
+        );
+
+        return new Category(
+            new CategoryName($name),
+            $attributeCollection
+        );
+    }
+}
